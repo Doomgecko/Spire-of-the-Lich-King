15b97e918e2812241ac158483875e5feb36fd280
Doomgecko <isabourin@millaschool.org>
2022-05-09 10:25:40 -0700

    Working on the javascript view updating

---
+++ SoLKScript.js
@@ -0,0 +1,32 @@
+let view = {
+    updatePlayer(){
+       let plrName = document.getElementById("playerName");
+       plrName.innerHTML = player.playerName;
+    },
+    updateMonster(){
+
+    },
+    updateMenu(){
+
+    }
+}
+
+let player = {
+    playerName: "Gygax",
+    playerHP: 0,
+    plrHPMax: 100,
+    playerMP: 0,
+    plrMPMax: 20,
+    playerSP: 0,
+    plrSPMax: 5
+}
+
+let monster = {
+    monsterName: "Minotaur",
+    monsterHP: 0,
+    mstrHPMax: 100,
+    mstrTelegraph: "Minotaur is ready to fight",
+    monsterActions: ["Attack", "SPAttack", "Defend", "Warcry"]
+}
+
+view.updatePlayer();
