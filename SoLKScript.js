28857daacbda4e76d35084556bf985c119203366
Doomgecko <isabourin@millaschool.org>
2022-05-11 08:59:42 -0700

    The view updates are now working properly and as intended!

--- SoLKScript.js
+++ SoLKScript.js
@@ -1,10 +1,21 @@
 let view = {
     updatePlayer(){
        let plrName = document.getElementById("playerName");
+       let plrHealth = document.getElementById("playerHP");
+       let plrMana = document.getElementById("playerMP");
+       let plrStamina = document.getElementById("playerSP");
        plrName.innerHTML = player.playerName;
+       plrHealth.innerHTML = "Health: " + player.playerHP + "/" + player.plrHPMax;
+       plrMana.innerHTML = "Mana: " + player.playerMP + "/" + player.plrMPMax;
+       plrStamina.innerHTML = "Stamina: " + player.playerSP + "/" + player.plrSPMax;
     },
     updateMonster(){
-
+        let mstrName = document.getElementById("monsterName");
+        let mstrHealth = document.getElementById("monsterHP");
+        let mstrTele = document.getElementById("monsterTelegraph");
+        mstrName.innerHTML = monster.monsterName;
+        mstrHealth.innerHTML = "Health: " + monster.monsterHP + "/" + monster.mstrHPMax;
+        mstrTele.innerHTML = monster.mstrTelegraph;
     },
     updateMenu(){
 
@@ -29,5 +40,5 @@ let monster = {
     monsterActions: ["Attack", "SPAttack", "Defend", "Warcry"]
 }
 
-
-
+view.updatePlayer();
+view.updateMonster();
